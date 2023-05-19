6fa7175914e854bdb841fc7eceaa18451bfbd38a
TranHuynhTanPhat <tranhuynhtanphat9380@gmail.com>
2023-05-19 15:01:43 +0700

    update

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://174.129.48.72/'
+axios.defaults.baseURL = 'http://localhost:8000/'
 axios.defaults.headers.common = { 'Authorization': `Bearer ${localStorage.getItem("access_token")}` }
 
 
