0f09b416de67799eba3720a10af19e9520785204
Võ Văn Nghĩa <127851778+vnghia109@users.noreply.github.com>
2023-05-25 14:10:02 +0700

    deploy to report

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://0.0.0.0:8000/'
+axios.defaults.baseURL = 'http://ec2-3-83-191-117.compute-1.amazonaws.com:8000/'
 axios.defaults.headers.common = { 'Authorization': `Bearer ${localStorage.getItem("access_token")}` }
 
 
